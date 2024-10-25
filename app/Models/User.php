bb608754a2393b5676d4a247aa8f10877d56d076
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-25 15:06:49 +0600

    added employer feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function employer(): HasOne
+    {
+        return $this->hasOne(Employer::class);
+    }
 }
