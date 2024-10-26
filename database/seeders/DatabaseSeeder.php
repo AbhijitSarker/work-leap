c8a3e27a59cc04dcda5f22f8446044ad8630d956
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-26 20:06:34 +0600

    added job apply functionality

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create([
             'name' => 'Abhijit Sarker',
             'email' => 'Abhijit@sarker.com',
+            'password' => Hash::make('password'), 
+
         ]);
         
         \App\Models\User::factory(300)->create();
