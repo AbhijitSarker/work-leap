289d5d7ceca15a4eaff482b930fdd03cdc0591b0
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-28 16:38:05 +0600

    added error handling and refactoring

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -43,7 +43,15 @@
       <p class="font-bold">Success!</p>
       <p>{{ session('success') }}</p>
     </div>
-    @endif
+  @endif
+
+  @if (session('error'))
+    <div role="alert"
+      class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+      <p class="font-bold">Error!</p>
+      <p>{{ session('error') }}</p>
+    </div>
+  @endif
   {{ $slot }}
 </body>
 
