30db07e7f3145202442492b3ef655a79f8d20aa5
AbhijitSarker <abhijitsarker03@gmail.com>
2024-10-31 15:58:30 +0600

    added functionality for editng jobs

--- resources/views/my_job/index.blade.php
+++ resources/views/my_job/index.blade.php
@@ -1,4 +1,3 @@
-
 <x-layout>
     <x-breadcrumbs :links="['My Jobs' => '#']" class="mb-4" />
         <div class="mb-8 text-right">
@@ -23,6 +22,11 @@
               @empty
                 <div>No applications yet</div>
               @endforelse
+
+              <div class="flex space-x-2">
+                <x-link-button href="{{ route('my-jobs.edit', $job) }}">Edit</x-link-button>
+              </div>
+              
             </div>
           </x-job-card>
         @empty
